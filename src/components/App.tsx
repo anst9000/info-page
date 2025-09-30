e15e16f8fe7f74d528b4e1d18e068643a9eb1d87
Anders Strömberg <coltlaboy@yahoo.se>
2025-09-30 08:37:32 +0100

    improve search filter

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,5 @@
-import { useEffect, useState } from "react"
+import { useMemo, useState } from "react"
+import { allInstructions } from "../data/instructions"
 import InstructionData from "./InstructionData"
 import InstructionList from "./InstructionList"
 
@@ -11,106 +12,18 @@ export type Instruction = {
   link?: string
 }
 
-const instructions: Array<Instruction> = [
-  {
-    id: 1,
-    name: "Tunnel till en miljö",
-    command: "tunnel [miljö]",
-    explanation:
-      "Kör kommando 'tunnel [miljö]' där det går att välja antingen test, int eller dev. Om man bara kör kommandot 'tunnel' är default-värdet test (sadbtestblip1).",
-    where: ".bash_functions",
-  },
-  {
-    id: 2,
-    name: "Emily",
-    command: "tunnel [miljö]",
-    explanation:
-      "Kör kommando 'tunnel [miljö]' där det går att välja antingen test, int eller dev. Default-miljön om man bara kör kommandot 'tunnel' är test (sadbtestblip1).",
-    where: ".bash_functions",
-    link: "http://www.dn.se",
-  },
-  {
-    id: 3,
-    name: "Michael",
-    command: "tunnel [miljö]",
-    explanation:
-      "Kör kommando 'tunnel [miljö]' där det går att välja antingen test, int eller dev. Default-miljön om man bara kör kommandot 'tunnel' är test (sadbtestblip1).",
-    where: ".bash_functions",
-    link: "http://www.dn.se",
-  },
-  {
-    id: 4,
-    name: "Sarah",
-    command: "tunnel [miljö]",
-    explanation:
-      "Kör kommando 'tunnel [miljö]' där det går att välja antingen test, int eller dev. Default-miljön om man bara kör kommandot 'tunnel' är test (sadbtestblip1).",
-    where: ".bash_functions",
-    link: "http://www.dn.se",
-  },
-  {
-    id: 5,
-    name: "David",
-    command: "tunnel [miljö]",
-    explanation:
-      "Kör kommando 'tunnel [miljö]' där det går att välja antingen test, int eller dev. Default-miljön om man bara kör kommandot 'tunnel' är test (sadbtestblip1).",
-    where: ".bash_functions",
-    link: "http://www.dn.se",
-  },
-  {
-    id: 6,
-    name: "Jessica",
-    command: "tunnel [miljö]",
-    explanation:
-      "Kör kommando 'tunnel [miljö]' där det går att välja antingen test, int eller dev. Default-miljön om man bara kör kommandot 'tunnel' är test (sadbtestblip1).",
-    where: ".bash_functions",
-    link: "http://www.dn.se",
-  },
-  {
-    id: 7,
-    name: "Daniel",
-    command: "tunnel [miljö]",
-    explanation:
-      "Kör kommando 'tunnel [miljö]' där det går att välja antingen test, int eller dev. Default-miljön om man bara kör kommandot 'tunnel' är test (sadbtestblip1).",
-    where: ".bash_functions",
-    link: "http://www.dn.se",
-  },
-  {
-    id: 8,
-    name: "Olivia",
-    command: "tunnel [miljö]",
-    explanation:
-      "Kör kommando 'tunnel [miljö]' där det går att välja antingen test, int eller dev. Default-miljön om man bara kör kommandot 'tunnel' är test (sadbtestblip1).",
-    where: ".bash_functions",
-    link: "http://www.dn.se",
-  },
-  {
-    id: 9,
-    name: "Matthew",
-    command: "tunnel [miljö]",
-    explanation:
-      "Kör kommando 'tunnel [miljö]' där det går att välja antingen test, int eller dev. Default-miljön om man bara kör kommandot 'tunnel' är test (sadbtestblip1).",
-    where: ".bash_functions",
-    link: "http://www.dn.se",
-  },
-  {
-    id: 10,
-    name: "Sophia",
-    command: "tunnel [miljö]",
-    explanation:
-      "Kör kommando 'tunnel [miljö]' där det går att välja antingen test, int eller dev. Default-miljön om man bara kör kommandot 'tunnel' är test (sadbtestblip1).",
-    where: ".bash_functions",
-    link: "http://www.dn.se",
-  },
-]
-
 function App() {
-  const [query, setQuery] = useState("")
-  const [currentInstructionId, setCurrentInstructionId] = useState(0)
-  const [filteredInstructions, setFilteredInstructions] = useState<
-    Array<Instruction>
-  >([])
+  const [query, setQuery] = useState<string>("")
+  const [currentInstructionId, setCurrentInstructionId] = useState<number>(0)
+  const [instructions] = useState<Array<Instruction>>(allInstructions)
 
-  const handleInputChange = (evnt: React.ChangeEvent<HTMLInputElement>) => {
+  const filteredInstructions = useMemo(() => {
+    return instructions.filter((instruction: Instruction) =>
+      instruction.name.toLowerCase().includes(query.toLowerCase())
+    )
+  }, [instructions, query])
+
+  function handleInputChange(evnt: React.ChangeEvent<HTMLInputElement>) {
     setQuery(evnt.target.value)
   }
 
@@ -118,20 +31,6 @@ function App() {
     console.log("clearInput")
     setQuery("")
   }
-
-  useEffect(() => {
-    let filteredInstructions: Array<Instruction> = []
-
-    if (query !== "") {
-      filteredInstructions = instructions.filter((instruction) =>
-        instruction.name.toLowerCase().includes(query.toLowerCase())
-      )
-    }
-
-    setFilteredInstructions(filteredInstructions)
-  }, [query])
-
-  console.log("--> currentInstructionId", currentInstructionId)
 
   return (
     <div className="container vt323-regular">
@@ -149,26 +48,26 @@ function App() {
         </button>
       </div>
 
-      <div className="search-result">
-        {/* {filteredUsers.length === 0 ? (
+      {/* {filteredUsers.length === 0 ? (
           <p>No users found</p>
-        ) : (
-          <ItemList items={filteredUsers} />
-        )} */}
+          ) : (
+            <ItemList items={filteredUsers} />
+            )} */}
 
-        {currentInstructionId !== 0 ? (
-          <InstructionData
-            instruction={instructions[currentInstructionId - 1]}
-            setCurrentInstructionId={setCurrentInstructionId}
-          />
-        ) : (
+      {currentInstructionId !== 0 ? (
+        <InstructionData
+          instruction={instructions[currentInstructionId - 1]}
+          setCurrentInstructionId={setCurrentInstructionId}
+        />
+      ) : (
+        <div className="search-result">
           <InstructionList
             instructions={filteredInstructions}
             query={query}
             setCurrentInstructionId={setCurrentInstructionId}
           />
-        )}
-      </div>
+        </div>
+      )}
     </div>
   )
 }
---
+++ src/data/instructions.ts
@@ -0,0 +1,237 @@
+import { Instruction } from "../components/App"
+
+export const allInstructions: Array<Instruction> = [
+  {
+    id: 1,
+    name: "Tunnel till en miljö",
+    command: "tunnel [miljö]",
+    explanation:
+      "Kör kommando 'tunnel [miljö]' där det går att välja antingen test, int eller dev. Om man bara kör kommandot 'tunnel' är default-värdet test (sadbtestblip1).",
+    where: ".bash_functions",
+  },
+  {
+    id: 2,
+    name: "Emily",
+    command: "tunnel [miljö]",
+    explanation:
+      "Kör kommando 'tunnel [miljö]' där det går att välja antingen test, int eller dev. Default-miljön om man bara kör kommandot 'tunnel' är test (sadbtestblip1).",
+    where: ".bash_functions",
+    link: "http://www.dn.se",
+  },
+  {
+    id: 3,
+    name: "Michael",
+    command: "tunnel [miljö]",
+    explanation:
+      "Kör kommando 'tunnel [miljö]' där det går att välja antingen test, int eller dev. Default-miljön om man bara kör kommandot 'tunnel' är test (sadbtestblip1).",
+    where: ".bash_functions",
+    link: "http://www.dn.se",
+  },
+  {
+    id: 4,
+    name: "Sarah",
+    command: "tunnel [miljö]",
+    explanation:
+      "Kör kommando 'tunnel [miljö]' där det går att välja antingen test, int eller dev. Default-miljön om man bara kör kommandot 'tunnel' är test (sadbtestblip1).",
+    where: ".bash_functions",
+    link: "http://www.dn.se",
+  },
+  {
+    id: 5,
+    name: "David",
+    command: "tunnel [miljö]",
+    explanation:
+      "Kör kommando 'tunnel [miljö]' där det går att välja antingen test, int eller dev. Default-miljön om man bara kör kommandot 'tunnel' är test (sadbtestblip1).",
+    where: ".bash_functions",
+    link: "http://www.dn.se",
+  },
+  {
+    id: 6,
+    name: "Jessica",
+    command: "tunnel [miljö]",
+    explanation:
+      "Kör kommando 'tunnel [miljö]' där det går att välja antingen test, int eller dev. Default-miljön om man bara kör kommandot 'tunnel' är test (sadbtestblip1).",
+    where: ".bash_functions",
+    link: "http://www.dn.se",
+  },
+  {
+    id: 7,
+    name: "Daniel",
+    command: "tunnel [miljö]",
+    explanation:
+      "Kör kommando 'tunnel [miljö]' där det går att välja antingen test, int eller dev. Default-miljön om man bara kör kommandot 'tunnel' är test (sadbtestblip1).",
+    where: ".bash_functions",
+    link: "http://www.dn.se",
+  },
+  {
+    id: 8,
+    name: "Olivia",
+    command: "tunnel [miljö]",
+    explanation:
+      "Kör kommando 'tunnel [miljö]' där det går att välja antingen test, int eller dev. Default-miljön om man bara kör kommandot 'tunnel' är test (sadbtestblip1).",
+    where: ".bash_functions",
+    link: "http://www.dn.se",
+  },
+  {
+    id: 9,
+    name: "Matthew",
+    command: "tunnel [miljö]",
+    explanation:
+      "Kör kommando 'tunnel [miljö]' där det går att välja antingen test, int eller dev. Default-miljön om man bara kör kommandot 'tunnel' är test (sadbtestblip1).",
+    where: ".bash_functions",
+    link: "http://www.dn.se",
+  },
+  {
+    id: 10,
+    name: "Sophia",
+    command: "tunnel [miljö]",
+    explanation:
+      "Kör kommando 'tunnel [miljö]' där det går att välja antingen test, int eller dev. Default-miljön om man bara kör kommandot 'tunnel' är test (sadbtestblip1).",
+    where: ".bash_functions",
+    link: "http://www.dn.se",
+  },
+  {
+    id: 11,
+    name: "Matthew",
+    command: "tunnel [miljö]",
+    explanation:
+      "Kör kommando 'tunnel [miljö]' där det går att välja antingen test, int eller dev. Default-miljön om man bara kör kommandot 'tunnel' är test (sadbtestblip1).",
+    where: ".bash_functions",
+    link: "http://www.dn.se",
+  },
+  {
+    id: 12,
+    name: "Sophia",
+    command: "tunnel [miljö]",
+    explanation:
+      "Kör kommando 'tunnel [miljö]' där det går att välja antingen test, int eller dev. Default-miljön om man bara kör kommandot 'tunnel' är test (sadbtestblip1).",
+    where: ".bash_functions",
+    link: "http://www.dn.se",
+  },
+  {
+    id: 13,
+    name: "Matthew",
+    command: "tunnel [miljö]",
+    explanation:
+      "Kör kommando 'tunnel [miljö]' där det går att välja antingen test, int eller dev. Default-miljön om man bara kör kommandot 'tunnel' är test (sadbtestblip1).",
+    where: ".bash_functions",
+    link: "http://www.dn.se",
+  },
+  {
+    id: 14,
+    name: "Sophia",
+    command: "tunnel [miljö]",
+    explanation:
+      "Kör kommando 'tunnel [miljö]' där det går att välja antingen test, int eller dev. Default-miljön om man bara kör kommandot 'tunnel' är test (sadbtestblip1).",
+    where: ".bash_functions",
+    link: "http://www.dn.se",
+  },
+  {
+    id: 15,
+    name: "Matthew",
+    command: "tunnel [miljö]",
+    explanation:
+      "Kör kommando 'tunnel [miljö]' där det går att välja antingen test, int eller dev. Default-miljön om man bara kör kommandot 'tunnel' är test (sadbtestblip1).",
+    where: ".bash_functions",
+    link: "http://www.dn.se",
+  },
+  {
+    id: 16,
+    name: "Sophia",
+    command: "tunnel [miljö]",
+    explanation:
+      "Kör kommando 'tunnel [miljö]' där det går att välja antingen test, int eller dev. Default-miljön om man bara kör kommandot 'tunnel' är test (sadbtestblip1).",
+    where: ".bash_functions",
+    link: "http://www.dn.se",
+  },
+  {
+    id: 17,
+    name: "Matthew",
+    command: "tunnel [miljö]",
+    explanation:
+      "Kör kommando 'tunnel [miljö]' där det går att välja antingen test, int eller dev. Default-miljön om man bara kör kommandot 'tunnel' är test (sadbtestblip1).",
+    where: ".bash_functions",
+    link: "http://www.dn.se",
+  },
+  {
+    id: 18,
+    name: "Sophia",
+    command: "tunnel [miljö]",
+    explanation:
+      "Kör kommando 'tunnel [miljö]' där det går att välja antingen test, int eller dev. Default-miljön om man bara kör kommandot 'tunnel' är test (sadbtestblip1).",
+    where: ".bash_functions",
+    link: "http://www.dn.se",
+  },
+  {
+    id: 19,
+    name: "Matthew",
+    command: "tunnel [miljö]",
+    explanation:
+      "Kör kommando 'tunnel [miljö]' där det går att välja antingen test, int eller dev. Default-miljön om man bara kör kommandot 'tunnel' är test (sadbtestblip1).",
+    where: ".bash_functions",
+    link: "http://www.dn.se",
+  },
+  {
+    id: 20,
+    name: "Sophia",
+    command: "tunnel [miljö]",
+    explanation:
+      "Kör kommando 'tunnel [miljö]' där det går att välja antingen test, int eller dev. Default-miljön om man bara kör kommandot 'tunnel' är test (sadbtestblip1).",
+    where: ".bash_functions",
+    link: "http://www.dn.se",
+  },
+  {
+    id: 21,
+    name: "Matthew",
+    command: "tunnel [miljö]",
+    explanation:
+      "Kör kommando 'tunnel [miljö]' där det går att välja antingen test, int eller dev. Default-miljön om man bara kör kommandot 'tunnel' är test (sadbtestblip1).",
+    where: ".bash_functions",
+    link: "http://www.dn.se",
+  },
+  {
+    id: 22,
+    name: "Sophia",
+    command: "tunnel [miljö]",
+    explanation:
+      "Kör kommando 'tunnel [miljö]' där det går att välja antingen test, int eller dev. Default-miljön om man bara kör kommandot 'tunnel' är test (sadbtestblip1).",
+    where: ".bash_functions",
+    link: "http://www.dn.se",
+  },
+  {
+    id: 23,
+    name: "Matthew",
+    command: "tunnel [miljö]",
+    explanation:
+      "Kör kommando 'tunnel [miljö]' där det går att välja antingen test, int eller dev. Default-miljön om man bara kör kommandot 'tunnel' är test (sadbtestblip1).",
+    where: ".bash_functions",
+    link: "http://www.dn.se",
+  },
+  {
+    id: 24,
+    name: "Sophia",
+    command: "tunnel [miljö]",
+    explanation:
+      "Kör kommando 'tunnel [miljö]' där det går att välja antingen test, int eller dev. Default-miljön om man bara kör kommandot 'tunnel' är test (sadbtestblip1).",
+    where: ".bash_functions",
+    link: "http://www.dn.se",
+  },
+  {
+    id: 25,
+    name: "Matthew",
+    command: "tunnel [miljö]",
+    explanation:
+      "Kör kommando 'tunnel [miljö]' där det går att välja antingen test, int eller dev. Default-miljön om man bara kör kommandot 'tunnel' är test (sadbtestblip1).",
+    where: ".bash_functions",
+    link: "http://www.dn.se",
+  },
+  {
+    id: 26,
+    name: "Sophia",
+    command: "tunnel [miljö]",
+    explanation:
+      "Kör kommando 'tunnel [miljö]' där det går att välja antingen test, int eller dev. Default-miljön om man bara kör kommandot 'tunnel' är test (sadbtestblip1).",
+    where: ".bash_functions",
+    link: "http://www.dn.se",
+  },
+]
